530ab34512e0da1e83363ae107ecdf484db708b6
RhysCrossley <43577505+RhysCrossley@users.noreply.github.com>
2018-10-30 17:17:39 +0000

    Updates

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class HealthSystem : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class OnDamagedEvent : UnityEvent<int> { }
+
+        public int health = 10;
+        public UnityEvent onDie;
+        public OnDamagedEvent onDamaged;
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            onDamaged.Invoke(health);
+            if (health < 1)
+            {
+                onDie.Invoke();
+            }
+        }
+    }
+
