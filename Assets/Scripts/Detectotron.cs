a87492e122cfd9154b86db5c2cc2a76cba048163
RhysCrossley <43577505+RhysCrossley@users.noreply.github.com>
2018-12-04 16:07:56 +0000

    Mr Bean detects then moves towards player

---
+++ Assets/Scripts/Detectotron.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+public class Detectotron : MonoBehaviour {
+
+    public UnityEvent onDetectotron;
+
+	private void OnTriggerEnter2D(Collider2D other)
+    {
+        //print("other " + other.transform);
+
+        onDetectotron.Invoke();
+    }
+}
